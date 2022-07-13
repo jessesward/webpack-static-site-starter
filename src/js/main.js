bfbf12895420442aa4b03910e2ce8f05d8bbddf8
Jesse Ward <jesseward@jessedev.local>
2022-07-13 09:24:59 -0700

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,1 @@
-$(function(){
-  var $readMoreLink = $(".read-more");
-
-  $readMoreLink.on("click", function(e){
-    e.preventDefault();
-    $(this).parent().next("div").show();
-    $(this).remove();
-  });
-});
+console.log("main.js")
